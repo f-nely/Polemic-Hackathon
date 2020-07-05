3847430915d7010c0ed53437a4fd6e8c45c09dab
nely de faria <nelly.nf54@gmail.com>
2020-07-05 18:40:04 -0300

    Include desafio-05.php

---
+++ src/desafio-05.php
@@ -0,0 +1,88 @@
+<?php
+
+$letra = readline("Informe uma letra: ");
+
+switch ($letra) {
+  case "a":
+    echo ". –";
+    break;
+  case "b":
+    echo "– . . .";
+    break;
+  case "c":
+    echo "– . – .";
+    break;
+  case "d":
+    echo "– . .";
+    break;
+  case "e":
+    echo ".";
+    break;
+  case "f":
+    echo ". . – .";
+    break;
+  case "g":
+    echo "– – .";
+    break;
+  case "h":
+    echo ". . . .";
+    break;
+  case "i":
+    echo "..";
+    break;
+  case "j":
+    echo ". – – –";
+    break;
+  case "k":
+    echo "– . –";
+    break;
+  case "l":
+    echo ". – . .";
+    break;
+  case "m":
+    echo "– –";
+    break;
+  case "n":
+    echo "– .";
+    break;
+  case "o":
+    echo "– – –";
+    break;
+  case "p":
+    echo ". – – .";
+    break;
+  case "q":
+    echo "– – . –	";
+    break;
+  case "r":
+    echo ". – .";
+    break;
+  case "s":
+    echo ". . .";
+    break;
+  case "t":
+    echo "–";
+    break;
+  case "u":
+    echo ". . –";
+    break;
+  case "v":
+    echo ". . . –";
+    break;
+  case "w":
+    echo ". – –";
+    break;
+  case "x":
+    echo "– . . –";
+    break;
+  case "y":
+    echo "– . – –";
+    break;
+  case "z":
+    echo "– – . .";
+    break;
+  default:
+    echo "Entrada inválida: ";
+    break;
+}
+
